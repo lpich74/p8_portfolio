4a8f4a01acd87d63dea418991b50cb8825ce9f62
Lucas PICHOLLET <Lucas@MacBook-Pro-de-Lucas.local>
2023-09-18 16:52:03 +0200

    P3, 4, 5 descriptions ajoutees

--- src/info/Info.js
+++ src/info/Info.js
@@ -93,7 +93,7 @@ export const info = {
             source: "https://github.com/lpich74/p6_kasa.git",
             image: mock1,
             description: {   
-                short: "This project consisted in developing the front end of a web application for a real estate agency by using React. A designer had previously taken care of the mockup with Figma.",
+                short: "This project consisted in developing the front end of a web application for a real estate agency using React. A designer had previously taken care of the mockup with Figma.",
                 issues: "At first, I had troubles understanding how the props work and also the different hooks. Also, I had to make sure I didn't mix up JS with JSX coding. Practising and seeking explanations through docs and experienced developers were the best ways to deal with these issues.",
                 developedSkills: [
                     "• Setting up navigation between the pages of the application using React Router",
@@ -124,12 +124,13 @@ export const info = {
             source: "https://github.com/lpich74/p2booki",
             image: mock3,
             description: {   
-                short: "",
-                issues: "",
+                short: "This project was my first task ever in my Web Developer course. I had to create the static home page for a travel agency using HTML and CSS. This page also had to be responsive.",
+                issues: "As my very first project, I encountered some issues, particularly with CSS. For instance, I faced problems with margins and paddings as some elements overlapped each other. After learning when to correctly use pixels and percentages, as well as mastering positioning properties, my work improved significantly.",
                 developedSkills: [
-                    "• ",
-                    "• ",
-                    "• "
+                    "• Implementing a responsive interface with HTML and CSS",
+                    "• Version controlling a project with Git and GitHub",
+                    "• Integrating content according to a mockup with HTML and CSS",
+                    "• Setting up a front end development environment"
                 ]
             }
         },
@@ -139,12 +140,12 @@ export const info = {
             source: "https://github.com/lpich74/p7_vieux_grimoire",
             image: mock4,
             description: {   
-                short: "",
-                issues: "",
+                short: "This project marked my introduction to back end development. While the front end code was pre-existing, my task was to develop a RESTful API using Node/Express.js and MongoDB. The primary goal was to enable authenticated users to perform CRUD (create, read, update, delete) operations on book data.",
+                issues: "At the outset, back end development appeared challenging, especially for someone primarily focused on front end work. To my pleasant surprise, I discovered it was not as daunting as it seemed, and I even found it enjoyable. I quickly grasped the concept of building middlewares, although my main challenge was ensuring their correct sequencing for flawless functionality.",
                 developedSkills: [
-                    "• ",
-                    "• ",
-                    "• "
+                    "• Implementing a data model in compliance with regulations",
+                    "• Securing data storage on MongoDB",
+                    "• Implementing CRUD operations securely"
                 ]
             }
         },
@@ -154,12 +155,12 @@ export const info = {
             source: "https://github.com/lpich74/p5n_carducci",
             image: mock5,
             description: {   
-                short: "",
-                issues: "",
+                short: "This project was slightly different, combining two distinct tasks into one. Initially, I tackled debugging an existing website. Then, I employed SEO tools like Google Lighthouse and Schema.org to optimize its performance.",
+                issues: "Not particularly difficult, this project actually was very educational. Optimizing a website for improved search engine rankings was a valuable learning experience. My main hurdle was optimizing images using 'srcset', as it seemed they were never quite optimized enough. However, I ultimately achieved a full-green report on Google Lighthouse for both the desktop and mobile versions.",
                 developedSkills: [
-                    "• ",
-                    "• ",
-                    "• "
+                    "• Optimizing the performance of a website",
+                    "• Debugging a website using Chrome DevTools",
+                    "• Writing a test plan for a website"
                 ]
             }
         },
