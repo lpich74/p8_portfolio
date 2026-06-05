fd9c481b3b97cd16e6c775fc7f52b0fc0f7a31f4
Lucas PICHOLLET <Lucas@MacBook-Pro-de-Lucas.local>
2026-06-05 17:25:14 +0200

    change to white background QR code

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,7 +6,7 @@ import EmojiBullet from "./EmojiBullet";
 import SocialIcon from "./SocialIcon";
 import {Box} from "@mui/material";
 import {info} from "../../info/Info";
-import qrCodeCV from "../../img/qrcodeCV.svg";
+import qrCodeCV from "../../img/qrcodeCV.png";
 
 export default function Home() {
 
